d3561cf726734df12831cb066d1aa40bb02bec07
kakuyogy <xuzhanddbb88@163.com>
2017-06-04 21:56:05 +0800

    go ❤️

--- dist/rx-simple.js
+++ dist/rx-simple.js
@@ -73,11 +73,83 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 6);
+/******/ 	return __webpack_require__(__webpack_require__.s = 7);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = Disposable;
+function Disposable(disposeFn) {
+    this.disposeFn = disposeFn;
+}
+
+Disposable.prototype.dispose = function () {
+    if (this.disposable instanceof Disposable) {
+        Disposable.dispose();
+    } else {
+        this.disposeFn();
+    }
+};
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = Observable;
+
+var _Observer = __webpack_require__(2);
+
+var _Observer2 = _interopRequireDefault(_Observer);
+
+var _Disposable = __webpack_require__(0);
+
+var _Disposable2 = _interopRequireDefault(_Disposable);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Observable(subscribeFn) {
+    this.subscribeFn = subscribeFn;
+}
+
+Observable.prototype.subscribe = function () {
+    var observer = void 0,
+        subscription = void 0;
+    if (arguments.length === 1 && arguments[0] instanceof _Observer2.default) {
+        // 传入的是Observer实例
+        observer = arguments[0];
+    } else {
+        observer = new _Observer2.default(arguments[0], arguments[1], arguments[2]);
+    }
+
+    subscription = this.subscribeFn(observer);
+
+    if (subscription instanceof _Disposable2.default) {
+        observer.unsubscribeFn = function () {
+            subscription.dispose();
+        };
+    }
+
+    return function () {
+        return observer.unsubscribe();
+    };
+};
+
+/***/ }),
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -135,7 +207,7 @@ Observer.prototype.unsubscribe = function () {
 };
 
 /***/ }),
-/* 1 */
+/* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -146,7 +218,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = create;
 
-var _Observable = __webpack_require__(5);
+var _Observable = __webpack_require__(1);
 
 var _Observable2 = _interopRequireDefault(_Observable);
 
@@ -157,7 +229,7 @@ function create(subscribeFn) {
 }
 
 /***/ }),
-/* 2 */
+/* 4 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -168,7 +240,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = create;
 
-var _Observer = __webpack_require__(0);
+var _Observer = __webpack_require__(2);
 
 var _Observer2 = _interopRequireDefault(_Observer);
 
@@ -179,17 +251,53 @@ function create(onNext, onError, onComplete) {
 }
 
 /***/ }),
-/* 3 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _Disposable = __webpack_require__(4);
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = interval;
+
+var _Observable = __webpack_require__(1);
+
+var _Observable2 = _interopRequireDefault(_Observable);
+
+var _Disposable = __webpack_require__(0);
 
 var _Disposable2 = _interopRequireDefault(_Disposable);
 
-var _Observable = __webpack_require__(5);
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function interval(period) {
+    var subscribeFn = function subscribeFn(observer) {
+        var count = 0;
+        var intervalId = window.setInterval(function () {
+            observer.next(count++);
+        }, period);
+
+        return new _Disposable2.default(function () {
+            window.clearInterval(intervalId);
+        });
+    };
+    return new _Observable2.default(subscribeFn);
+}
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _Disposable = __webpack_require__(0);
+
+var _Disposable2 = _interopRequireDefault(_Disposable);
+
+var _Observable = __webpack_require__(1);
 
 var _Observable2 = _interopRequireDefault(_Observable);
 
@@ -219,97 +327,25 @@ _Observable2.default.prototype.take = function (count) {
 };
 
 /***/ }),
-/* 4 */
+/* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = Disposable;
-function Disposable(disposeFn) {
-    this.disposeFn = disposeFn;
-}
-
-Disposable.prototype.dispose = function () {
-    if (this.disposable instanceof Disposable) {
-        Disposable.dispose();
-    } else {
-        this.disposeFn();
-    }
-};
-
-/***/ }),
-/* 5 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = Observable;
-
-var _Observer = __webpack_require__(0);
-
-var _Observer2 = _interopRequireDefault(_Observer);
-
-var _Disposable = __webpack_require__(4);
-
-var _Disposable2 = _interopRequireDefault(_Disposable);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function Observable(subscribeFn) {
-    this.subscribeFn = subscribeFn;
-}
-
-Observable.prototype.subscribe = function () {
-    var observer = void 0,
-        subscription = void 0;
-    if (arguments.length === 1 && arguments[0] instanceof _Observer2.default) {
-        // 传入的是Observer实例
-        observer = arguments[0];
-    } else {
-        observer = new _Observer2.default(arguments[0], arguments[1], arguments[2]);
-    }
-
-    subscription = this.subscribeFn(observer);
-
-    if (subscription instanceof _Disposable2.default) {
-        observer.unsubscribeFn = function () {
-            subscription.dispose();
-        };
-    }
-
-    return function () {
-        return observer.unsubscribe();
-    };
-};
-
-/***/ }),
-/* 6 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var _create = __webpack_require__(1);
+var _create = __webpack_require__(3);
 
 var _create2 = _interopRequireDefault(_create);
 
-var _create3 = __webpack_require__(2);
+var _create3 = __webpack_require__(4);
 
 var _create4 = _interopRequireDefault(_create3);
 
-var _interval = __webpack_require__(7);
+var _interval = __webpack_require__(5);
 
 var _interval2 = _interopRequireDefault(_interval);
 
-__webpack_require__(3);
+__webpack_require__(6);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -325,42 +361,6 @@ Rx.Observer = {
 };
 
 module.exports = Rx;
-
-/***/ }),
-/* 7 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = interval;
-
-var _Observable = __webpack_require__(5);
-
-var _Observable2 = _interopRequireDefault(_Observable);
-
-var _Disposable = __webpack_require__(4);
-
-var _Disposable2 = _interopRequireDefault(_Disposable);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function interval(period) {
-    var subscribeFn = function subscribeFn(observer) {
-        var count = 0;
-        var intervalId = window.setInterval(function () {
-            observer.next(count++);
-        }, period);
-
-        return new _Disposable2.default(function () {
-            window.clearInterval(intervalId);
-        });
-    };
-    return new _Observable2.default(subscribeFn);
-}
 
 /***/ })
 /******/ ]);
